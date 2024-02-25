8d5179a47fd6970962eff8c3ba4ad9c28881015b
phongnguyenrvn1995 <61311043+phongnguyenrvn1995@users.noreply.github.com>
2024-02-25 21:04:41 +0700

    Imports and Exports

--- My-Expo-Prj/App.js
+++ My-Expo-Prj/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { M1, M2 } from './my_libs'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {M2()}
       <StatusBar style="auto" />
     </View>
   );
---
+++ My-Expo-Prj/my_libs/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const M1 = () => {
+    return (<View>
+        <Text>M1</Text>
+    </View>);
+};
+
+const M2 = () => {
+    return (<View>
+        <Text>M2</Text>
+    </View>);
+};
+
+export {M1, M2};
