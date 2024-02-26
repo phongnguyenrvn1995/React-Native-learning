ad50c0c0c43eff6aed4fbf2fa9f9ff39fb646d1c
phongnguyenrvn1995 <61311043+phongnguyenrvn1995@users.noreply.github.com>
2024-02-26 23:11:47 +0700

    Spread

--- My-Expo-Prj/App.js
+++ My-Expo-Prj/App.js
@@ -4,16 +4,19 @@ import { M1, M2 } from './my_libs'
 import { y1, z } from './ArrowFunc';
 import { Cat } from './MyClasses';
 import { age, city, hairColor, myArray, myArray1, myArray2, myArray3, myArray4, myObject, name, name1, others, printMyArray, printMyObject } from './Destructing';
+import { myArr, myArr2, myObj1, myObj2 } from './Spread';
 
 export default function App() {
   let cat = new Cat('Meo', 1);
   console.log(cat);
   console.log(name1, hairColor, city, others);
+  console.log(myObj1);
+  console.log(myObj2);
   return (
     <View style={styles.container}>
       {M2()}
-      <Text>{printMyObject(myObject)}</Text>
-      <Text>{printMyArray(myArray)}</Text>
+      <Text>{myArr}</Text>
+      <Text>{myArr2}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ My-Expo-Prj/Spread.js
@@ -0,0 +1,5 @@
+export const myArr = [3, 4, 5];
+export const myArr2 = [1, 2, ...myArr, 6, 7];
+
+export const myObj1 = {name: 'Phong', hairColor: 'black'};
+export const myObj2 = {...myObj1, age: 29};
